5fa074e38f6bd3d34c9ef7b168476df2a49121a3
YasminMSoares <158000307+YasminMSoares@users.noreply.github.com>
2025-09-13 13:22:40 -0300

    Update CadastroUsuario.hs

--- CadastroUsuario.hs
+++ CadastroUsuario.hs
@@ -1,1 +1,15 @@
+module CadastroUsuario where
 
+Import Types (Usuario)
+
+adicionarUsuario :: [Usuario] -> Usuario -> [Usuario]
+adicionarUsuario lista novo = novo : lista
+
+removerUsuario :: [Usuario] -> String -> [Usuario]
+removerUsuario lista mat = filter (\u -> matricula u /= mat) lista
+
+listarUsuarios :: [Usuario] -> _ -> IO ()
+listarUsuarios [] nome = putStrLn "Nenhum usuário cadastrado."
+listarUsuarios usuarios nome = do
+  putStrLn "\nLista de usuários:"
+  mapM_ (\u -> putStrLn $ "Nome: " ++ nome u ++ ", Matrícula: " ++ matricula u ++ ", Email: " ++ email u) usuarios
